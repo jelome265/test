fc18a40de189ab95317cafb169eabaad1897cc67
jelome265 <130139884+jelome265@users.noreply.github.com>
2026-05-08 19:23:29 +0200

    fixed?

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,8 +1,8 @@
-const reactNativePreset = require('./node_modules/react-native/jest-preset');
+const reactNativePreset = require('react-native/jest-preset');
 
 module.exports = {
   ...reactNativePreset,
-  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
